23134a08a1f6107e2926ff9a1f8a047eeb761717
daanvr <daanvr@gmail.com>
2018-05-10 18:34:19 +0200

    Today data emphasis - playing with css and html to clean up infobox

--- script.js
+++ script.js
@@ -37,6 +37,21 @@ map.on('load', function(e) {
     }, 'country-label-lg'); // Place polygon under this labels.
 
     map.addLayer({
+        "id": "routes-today",
+        "type": "line",
+        "source": "Scotland-Routes",
+        "layout": {
+        
+        },
+        "paint": {
+            "line-color": "#bbbbbb",
+            "line-width": 3,
+            "line-gap-width": 1
+
+        }
+    }, 'photos'); // Place polygon under this labels.
+
+    map.addLayer({
         "id": "routes",
         "type": "line",
         "source": "Scotland-Routes",
@@ -48,7 +63,8 @@ map.on('load', function(e) {
             "line-width": 2,
             "line-gap-width": 1
         }
-    }, 'photos'); // Place polygon under this labels.
+    }, 'routes-today'); // Place polygon under this labels.
+
 
 
 //changes cursor style when on clickable layer.
@@ -64,28 +80,28 @@ map.on('click', function (e) {
 
 //If clicked on a photo, display photo info 
     if (feature.properties.type == "Foto") {
-        document.getElementById('infobox_name').innerHTML = (feature.properties.FileName);
+        // document.getElementById('infobox_name').innerHTML = (feature.properties.FileName);
         if (feature.properties.URLsmall != undefined) {document.getElementById('infobox_img').setAttribute('src', feature.properties.URLsmall); };
         document.getElementById('Photo-Big').setAttribute('src', feature.properties.URL);
-        if (feature.properties.CreateTime != undefined) {document.getElementById('infobox_info').innerHTML = ("Time: " + feature.properties.CreateTime + "<br>" + "Date: " + feature.properties.CreateDate);
-        } else {
-          document.getElementById('infobox_info').innerHTML = ""; 
-        }
+        // if (feature.properties.CreateTime != undefined) {document.getElementById('infobox_info').innerHTML = ("Time: " + feature.properties.CreateTime + "<br>" + "Date: " + feature.properties.CreateDate);
+        // } else {
+        //   document.getElementById('infobox_info').innerHTML = ""; 
+        // }
     }
 
 //If clicked on a random place on the map, display default info    
     else if (feature.properties.type == undefined) {
-        document.getElementById('infobox_name').innerHTML = ("Tonny's reis door Schotland");
-        document.getElementById('infobox_info').innerHTML = ("Dit is een interactive kaart van Tonny's reis door Schotland, in April van 2018, ten eren van haar 60ste verjaardag. Deze reis is gefinancierd door familie &amp vrienden als een cadeau voor deze heugelijke mijlpaal.")
+        // document.getElementById('infobox_name').innerHTML = ("Tonny's reis door Schotland");
+        // document.getElementById('infobox_info').innerHTML = ("Dit is een interactive kaart van Tonny's reis door Schotland, in April van 2018, ten eren van haar 60ste verjaardag. Deze reis is gefinancierd door familie &amp vrienden als een cadeau voor deze heugelijke mijlpaal.")
         document.getElementById('infobox_img').setAttribute('src', "https://static1.squarespace.com/static/5846811b5016e18b4f9999a2/t/58481cc5e58c6289807c1619/1481121486133/?format=400w");
         document.getElementById('Photo-Big').setAttribute('src', "https://static1.squarespace.com/static/5846811b5016e18b4f9999a2/t/58481cc5e58c6289807c1619/1481121486133/?format=600w");
     }
 });
 
 // makes HTML button clickable, with js consequenses
-document.getElementById("btn-next").addEventListener('click', function(e) {
-    //put js consequenses here:
-});
+// document.getElementById("btn-next").addEventListener('click', function(e) {
+//     //put js consequenses here:
+// });
 
 //adds map controls to the map
 map.addControl(new mapboxgl.NavigationControl());
@@ -96,27 +112,30 @@ function filterBy(SliderValue){
     var slider = document.getElementById("slider-start").value;
     var datephotos  //Initalise date var
     var dateroutes  //Initalise dateroutes var
+    var dateroutesfrom  //Initalise dateroutes var
 
-    //translates the range slider steps to dates 
-         if (slider == 1) {datephotos = "2018-04-06"; dateroutes = "20180407";}
-    else if (slider == 2) {datephotos = "2018-04-07"; dateroutes = "20180408";}
-    else if (slider == 3) {datephotos = "2018-04-08"; dateroutes = "20180409";}
-    else if (slider == 4) {datephotos = "2018-04-09"; dateroutes = "20180410";}
-    else if (slider == 5) {datephotos = "2018-04-10"; dateroutes = "20180411";}
-    else if (slider == 6) {datephotos = "2018-04-11"; dateroutes = "20180412";}
-    else if (slider == 7) {datephotos = "2018-04-12"; dateroutes = "20180413";}
-    else if (slider == 8) {datephotos = "2018-04-13"; dateroutes = "20180414";}
-    else if (slider == 9) {datephotos = "2018-04-14"; dateroutes = "20180415";}
-    else if (slider == 10) {datephotos = "2018-04-15"; dateroutes = "20180416";}
+    //translates the range slider steps to dates for photos routes and routesday before
+         if (slider == 1) {datephotos = "2018-04-06"; dateroutes = "20180407"; dateroutesfrom = "20180406";}
+    else if (slider == 2) {datephotos = "2018-04-07"; dateroutes = "20180408"; dateroutesfrom = "20180407";}
+    else if (slider == 3) {datephotos = "2018-04-08"; dateroutes = "20180409"; dateroutesfrom = "20180408";}
+    else if (slider == 4) {datephotos = "2018-04-09"; dateroutes = "20180410"; dateroutesfrom = "20180409";}
+    else if (slider == 5) {datephotos = "2018-04-10"; dateroutes = "20180411"; dateroutesfrom = "20180410";}
+    else if (slider == 6) {datephotos = "2018-04-11"; dateroutes = "20180412"; dateroutesfrom = "20180411";}
+    else if (slider == 7) {datephotos = "2018-04-12"; dateroutes = "20180413"; dateroutesfrom = "20180412";}
+    else if (slider == 8) {datephotos = "2018-04-13"; dateroutes = "20180414"; dateroutesfrom = "20180413";}
+    else if (slider == 9) {datephotos = "2018-04-14"; dateroutes = "20180415"; dateroutesfrom = "20180414";}
+    else if (slider == 10) {datephotos = "2018-04-15"; dateroutes = "20180416"; dateroutesfrom = "20180415";}
     
     //Actual Filtering by date
     if (slider > 0 && slider < 11) {
         map.setFilter('photos', ["==", "CreateDate", datephotos]);
         map.setFilter('routes', ["<=", "startTime", dateroutes]);
+        map.setFilter('routes-today', ["all", ["<=", "startTime", dateroutes], [">=", "startTime", dateroutesfrom]]);
     }
     else {
         map.setFilter('photos', null);
         map.setFilter('routes', null);
+        map.setFilter('routes-today', ["<=", "startTime", 1]);
     }
     
     //Filtering confimration in console
@@ -131,8 +150,12 @@ $(document).ready(function() {
     console.log("ready!");
 
 
+
     //make sure map is done loading, eg all the layers exist
     map.on("load", function initiatefilter() {
+        //Initiate Filter
+        filterBy("" + 0 + "");
+
         //add event listener to HTML range slider
         document.getElementById('slider-start').addEventListener('input', function(e) {
             //code to be executed when event listener is triggerd
