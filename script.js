f01b14c03355e89b00ee23a9d0d30692b8121f95
daanvr <daanvr@gmail.com>
2018-05-18 14:22:41 +0200

    Cleaned up java script file considerably
    - Improving readability by cleaning unnecessary code, empty space and
    comments
    - writing a lot of new comments
    - moving bits of code into functions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+//Set global variables
 var photoDB;
+
+//load json file as static databse and stores the data in the "photoDB" js object
+var photoDB;
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "https://daanvr.github.io/Schotland/photoDB.json", true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) { photoDB = JSON.parse(this.responseText);}
+};
 
 //Mapbox initalisation
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFhbnZyIiwiYSI6ImNpdTJmczN3djAwMHEyeXBpNGVndWtuYXEifQ.GYZf7r9gTfQL3W-GpmmJ3A';
@@ -11,80 +21,21 @@ var map = new mapboxgl.Map({
     pitch: 0.5,
 });
 
-map.on('load', function(e) {
+//adds map controls to the map
+map.addControl(new mapboxgl.NavigationControl());
 
-//load geojson files
-    map.addSource('Scotland-Foto', {
-        "type": "geojson",
-        "data": "https://daanvr.github.io/Schotland/Scotrip-FotoDataFile-RichOnly-Live.geojson"
-    });
-    map.addSource('Scotland-Routes', {
-        "type": "geojson",
-        "data": "https://daanvr.github.io/Schotland/Routes.geojson"
-    });
-
-    map.addLayer({
-        "id": "photos",
-        "type": "symbol",
-        "source": "Scotland-Foto",
-        "layout": {
-            "icon-image": "attraction-15",
-            "icon-size": 1.25,
-            "icon-padding": 0,
-            "icon-allow-overlap":true
-        },
-        "paint": {
-            "icon-opacity": 1
-        }
-    }, 'country-label-lg'); // Place polygon under this labels.
-
-    map.addLayer({
-        "id": "routes-today",
-        "type": "line",
-        "source": "Scotland-Routes",
-        "layout": {
-        
-        },
-        "paint": {
-            "line-color": "#4285F4",
-            "line-width": 6
-
-        }
-    }, 'photos'); // Place polygon under this labels.
-
-    map.addLayer({
-        "id": "routes",
-        "type": "line",
-        "source": "Scotland-Routes",
-        "layout": {
-        
-        },
-        "paint": {
-            "line-color": "rgba(120, 180, 244, 1)",
-            "line-width": 4
-        }
-    }, 'routes-today'); // Place polygon under this labels.
-
-    map.addLayer({
-        "id": "routes-shadow",
-        "type": "line",
-        "source": "Scotland-Routes",
-        "layout": {
-        
-        },
-        "paint": {
-            "line-color": "#4285F4",
-            "line-width": 6
-        }
-    }, 'routes'); // Place polygon under this labels.
-
-
-//changes cursor style when on clickable layer.
-    map.on("mousemove", "photos", function(e) {map.getCanvas().style.cursor = 'pointer';});
-    map.on('mouseleave', "photos", function() {map.getCanvas().style.cursor = '';});
+//execute these startup functions when map is ready
+map.on("load", function initiatefilter() {
+    LoadGEOJsonSources();  //this loads the geojson sources, then loads the layser and then makes the mouse currsor change when hovering the "clickable" layers
+    filterBy("" + 0 + "");  //Initiate Filter
+    NewSelection(64);  //Initiates selected img
+    LoadCarouselImgs();  //loads imgs in carousel
+    SliderListener();  //Call function to set event lisner to html for the day range selector
 });
 
-//this makes map features clickable and puts the data in theinfo box
+
+//Make map features clickable
+//& puts the data in theinfo box
 map.on('click', function (e) {
     var features = map.queryRenderedFeatures(e.point, {});
     if (!features.length) {return;}
@@ -99,54 +50,15 @@ map.on('click', function (e) {
         searchphotoDB(feature.properties.FileName);
         console.log("name: " + feature.properties.FileName)
     }
-
-    //If clicked on a random place on the map, display default info    
-    else if (feature.properties.type == undefined) {
-        document.getElementById('infobox_img').setAttribute('src', "");
-        document.getElementById('Photo-Big').setAttribute('src', "");
-    }
-
 });
 
-// makes HTML button clickable, with js consequenses
-document.getElementById("nextph").addEventListener('click', function(e) {
-    selectedphoto.nbr = selectedphoto.nbr + 1;
-    NewSelection(selectedphoto.nbr);
-});
-
-document.getElementById("prevph").addEventListener('click', function(e) {
-    selectedphoto.nbr = selectedphoto.nbr - 1;
-    NewSelection(selectedphoto.nbr);
-});
-
-document.getElementById("locph").addEventListener('click', function(e) {
-    Long = photoDB[selectedphoto.nbr].Longitude;
-    Lat = photoDB[selectedphoto.nbr].Latitude;
-    Fly(Long, Lat);
-});
-
-document.getElementById("bigph").addEventListener('click', function(e) {
-    document.getElementById('Photo-Big-Background').style.display = 'inline';
-});
-
-function Fly(Long, Lat){
-    map.flyTo({
-        center: [Long, Lat],
-        zoom: (10)
-    });
-};
-
-//adds map controls to the map
-map.addControl(new mapboxgl.NavigationControl());
-
-//Filter function
+//Function to filter map
 function filterBy(SliderValue){
-    //get range slider valure from HTML
-    var slider = document.getElementById("slider-start").value;
+    var slider = document.getElementById("slider-start").value;  //get range slider valure from HTML
     var datephotos  //Initalise date var
     var dateroutesto  //Initalise dateroutes var
     var dateroutesfrom  //Initalise dateroutes var
-    var currentday
+    var currentday //Initatlise current day var, containing the naming of every day
 
     //arrays of relevant valies compared to slider value. eg slider position 4 of datephotos = 2018-04-09 and position 4 of currentday = Maandag 
     datephotos =        ["",            "2018-04-06",   "2018-04-07",   "2018-04-08",   "2018-04-09",   "2018-04-10",   "2018-04-11",   "2018-04-12",   "2018-04-13",   "2018-04-14",   "2018-04-15",   ""          ];
@@ -156,77 +68,51 @@ function filterBy(SliderValue){
     FistPhoto =         ["",            "",             "250",          "500",          "750",          "1000",         "1150",         "1400",         "1500",         "1800",         "1950",         ""];
 
     //exectution of filter by date
-    if (slider > 0 && slider < 11) {
+    if (slider > 0 && slider < 11) { 
+        //Slider is used to filter map elleemnts 
         map.setFilter('photos', ["==", "CreateDate", datephotos[slider]]);
         map.setFilter('routes', ["<=", "startTime", dateroutesto[slider]]);
         map.setFilter('routes-shadow', ["<=", "startTime", dateroutesto[slider]]);
         map.setFilter('routes-today', ["all", ["<=", "startTime", dateroutesto[slider]], [">=", "startTime", dateroutesfrom[slider]]]);
-        //NewSelection(FistPhoto);
-    }
-    else {
+    } else { 
+        //Else slider is off
         map.setFilter('photos', null);
         map.setFilter('routes', null);
         map.setFilter('routes-shadow', null);
         map.setFilter('routes-today', ["<=", "startTime", 1]);
     }
-    //set Day Lable to correct day 
-    document.getElementById("daylable").textContent = currentday[slider];
-    //Filtering confimration in console
-    console.log("Filtering: " + currentday[slider]) 
+  
+    document.getElementById("daylable").textContent = currentday[slider];  //set Day Lable to correct day 
+    console.log("Filtering: " + currentday[slider]); //Filtering confimration in console
+
 };
 
-//make sure map is done loading, eg all the layers exist
-map.on("load", function initiatefilter() {
-    //Initiate Filter
-    filterBy("" + 0 + "");
-    NewSelection(64);
-    //add event listener to HTML range slider
-    document.getElementById('slider-start').addEventListener('input', function(e) {
-        //code to be executed when event listener is triggerd
-        var SliderValue = "" + parseInt(e.target.value, 10) + "" //create vare with range slider value
-        filterBy(SliderValue);  //trigger Fiter function adn send varibale with it.
-   });
-
-        var i;
-    for (i = 0; i < 2003; i++) { 
-        var newdiv = document.createElement('div');
-        var position = i;
-        newdiv.className = 'carousel-containter img' + position;
-        newdiv.Name = i;
-        newdiv.innerHTML = '<img class="carousel-img img' + position + '" onclick="NewSelection(' + i + ')"  name="' + i + '"src="' + photoDB[i].URLsmall + '">';
-        newdiv.idName = 'img' + position;
-        document.getElementById('main-carousel').appendChild(newdiv);
-    };
-
-});
-
-//load json file as static databse and store data in the photoDB var
-var photoDB;
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "https://daanvr.github.io/Schotland/photoDB.json", true);
-xmlhttp.send();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) { photoDB = JSON.parse(this.responseText);}
-};
-
-
-
-
-
-
+//function used to set a new selection. it upadates the img in the top left, set slider day filter to 0 and logs selected img info
 var prevphoto = {};
 var selectedphoto = {};
 var nextphoto = {};
 var carouselphotos = {};
 function NewSelection(newmainphotonbr){
-    //set slider to the day corresponding to the photo
+    //set slider to all days
     var SliderValue = 0;
     document.getElementById('slider-start').value = SliderValue;
     filterBy(SliderValue);
 
-    //makes a loop to add all img to carousel    
+    //load varibales from new selected photo to 
+    selectedphoto = {nbr:newmainphotonbr, URLsmall:"", URL:""};
+    selectedphoto.URLsmall = photoDB[selectedphoto.nbr].URLsmall;
+    selectedphoto.URL = photoDB[selectedphoto.nbr].URL;
+    selectedphoto.latitude = photoDB[selectedphoto.nbr].Latitude;
+    selectedphoto.longitude = photoDB[selectedphoto.nbr].Longitude;
 
+    //set variables from new selection to UI 
+    document.getElementById('infobox_img').setAttribute('src', selectedphoto.URLsmall);
+    document.getElementById('Photo-Big').setAttribute('src', selectedphoto.URL);
 
+    //call function to write in consol the variable from new selection
+    writephotovars();
+
+    //this code was temporarly used to expiriment with other valuse than photos in carousel. This might be usefull in the future
         // var newdiv = document.createElement('div');
         // var position = i - newmainphotonbr;
         // newdiv.className = 'carousel-containter img' + position + ' carousel-info';
@@ -234,44 +120,15 @@ function NewSelection(newmainphotonbr){
         // newdiv.innerHTML = '<div class="carousel-info-text"><p>Woensdag</p></div>';
         // newdiv.idName = 'img' + position;
         // document.getElementById('main-carousel').appendChild(newdiv);
-
-    // for (i = newmainphotonbr + 4; i < newmainphotonbr+15; i++) { 
-    //         var newdiv = document.createElement('div');
-    //     var position = i - newmainphotonbr;
-    //     newdiv.className = 'carousel-containter img' + position;
-    //     newdiv.Name = i;
-    //     newdiv.innerHTML = '<img class="carousel-img img' + position + '" onclick="NewSelection(' + i + ')"  name="' + i + '"src="' + photoDB[i].URLsmall + '">';
-    //     newdiv.idName = 'img' + position;
-    //     document.getElementById('main-carousel').appendChild(newdiv);
-    // };
-    
-    selectedphoto = {nbr:newmainphotonbr, URLsmall:"", URL:""};
-    selectedphoto.URLsmall = photoDB[selectedphoto.nbr].URLsmall;
-    selectedphoto.URL = photoDB[selectedphoto.nbr].URL;
-    selectedphoto.latitude = photoDB[selectedphoto.nbr].Latitude;
-    selectedphoto.longitude = photoDB[selectedphoto.nbr].Longitude;
-
-    writephotovars();
-
-    //document.getElementById('ph1').setAttribute('src', prevphoto.URL);
-    document.getElementById('infobox_img').setAttribute('src', selectedphoto.URLsmall);
-    document.getElementById('Photo-Big').setAttribute('src', selectedphoto.URL);
-    //document.getElementById('ph3').setAttribute('src', nextphoto.URL);
-    // document.getElementById('selectedimg').
 };
 
-
-
-
-
-
+// function used to log the selected foto info to the consol
 function writephotovars(){
     console.log("selected photo info:");
     console.log(selectedphoto);
 };
 
-
-
+// funciton to search the phtoo database for the picure corresponding to the clicked map icone corresponging to a specific img.
 function searchphotoDB(searchquery){
     console.log("searching")
     for (i = 0; i < 2003; i++) {
@@ -282,6 +139,144 @@ function searchphotoDB(searchquery){
         }
     };
 };
+
+//loads all imgs in carousel
+function LoadCarouselImgs(){
+        var i;
+    for (i = 0; i < 2003; i++) { 
+        var newdiv = document.createElement('div');
+        var position = i;
+        newdiv.className = 'carousel-containter img' + position;
+        newdiv.Name = i;
+        newdiv.innerHTML = '<img class="carousel-img img' + position + '" onclick="NewSelection(' + i + ')"  name="' + i + '"src="' + photoDB[i].URLsmall + '">';
+        newdiv.idName = 'img' + position;
+        document.getElementById('main-carousel').appendChild(newdiv);
+    };
+}
+
+//function to add event listener to HTML range slider to be used as a day selector
+function SliderListener() {
+    document.getElementById('slider-start').addEventListener('input', function(e) {
+        //code to be executed when event listener is triggerd
+        var SliderValue = "" + parseInt(e.target.value, 10) + "" //create vare with range slider value
+        filterBy(SliderValue);  //trigger Fiter function adn send varibale with it.
+   });
+};
+
+//Function to simplify Fly To functionalisty of Mapbox
+function Fly(Long, Lat){
+    map.flyTo({
+        center: [Long, Lat],
+        zoom: (10)
+    });
+};
+
+HTMLbtnsClick();
+function HTMLbtnsClick() {
+// makes HTML button clickable, with js consequenses
+    document.getElementById("nextph").addEventListener('click', function(e) {
+        selectedphoto.nbr = selectedphoto.nbr + 1;
+        NewSelection(selectedphoto.nbr);
+    });
+
+    document.getElementById("prevph").addEventListener('click', function(e) {
+        selectedphoto.nbr = selectedphoto.nbr - 1;
+        NewSelection(selectedphoto.nbr);
+    });
+
+    document.getElementById("locph").addEventListener('click', function(e) {
+        Long = photoDB[selectedphoto.nbr].Longitude;
+        Lat = photoDB[selectedphoto.nbr].Latitude;
+        Fly(Long, Lat);
+    });
+
+    document.getElementById("bigph").addEventListener('click', function(e) {
+        document.getElementById('Photo-Big-Background').style.display = 'inline';
+    });
+};
+
+//Loading geojson sources for layers to display them
+function LoadGEOJsonSources() {
+    map.addSource('Scotland-Foto', {
+        "type": "geojson",
+        "data": "https://daanvr.github.io/Schotland/Scotrip-FotoDataFile-RichOnly-Live.geojson"
+    });
+    map.addSource('Scotland-Routes', {
+        "type": "geojson",
+        "data": "https://daanvr.github.io/Schotland/Routes.geojson"
+    });
+
+    DisplayGEOJsonLayers();  //Now that the sources are loaded, have the Layers loaded
+};
+
+//Displaying geojson data from the previously laoded sources
+function DisplayGEOJsonLayers(){
+    map.addLayer({ 
+        //photos layer
+        "id": "photos",
+        "type": "symbol",
+        "source": "Scotland-Foto",
+        "layout": {
+            "icon-image": "attraction-15",
+            "icon-size": 1.25,
+            "icon-padding": 0,
+            "icon-allow-overlap":true
+        },
+        "paint": {
+            "icon-opacity": 1
+        }
+    }, 'country-label-lg'); // Place polygon under this labels.
+
+    map.addLayer({ 
+        //selected rout section layer
+        "id": "routes-today",
+        "type": "line",
+        "source": "Scotland-Routes",
+        "layout": {
+        },
+        "paint": {
+            "line-color": "#4285F4",
+            "line-width": 6
+        }
+    }, 'photos'); // Place polygon under this labels.
+
+    map.addLayer({
+        //rout layer
+        "id": "routes",
+        "type": "line",
+        "source": "Scotland-Routes",
+        "layout": {
+        },
+        "paint": {
+            "line-color": "rgba(120, 180, 244, 1)",
+            "line-width": 4
+        }
+    }, 'routes-today'); // Place polygon under this labels.
+
+    map.addLayer({
+        //layer used to create lins(borders) arround rout
+        "id": "routes-shadow",
+        "type": "line",
+        "source": "Scotland-Routes",
+        "layout": {
+        },
+        "paint": {
+            "line-color": "#4285F4",
+            "line-width": 6
+        }
+    }, 'routes'); // Place polygon under this labels.
+
+    ClickbleMapItemCursor(); //Now that the layers a loaded, have the mouse cursor change when hovering some of the layers
+};
+
+//making "clickble" layers on the map change the mouse cursor. This helps the user see an map item is clickble.
+function ClickbleMapItemCursor(){
+    //changes cursor style when hovering "clickable" layer.
+    map.on("mousemove", "photos", function(e) {map.getCanvas().style.cursor = 'pointer';});
+    map.on('mouseleave', "photos", function() {map.getCanvas().style.cursor = '';});
+
+};
+
 
 
 
